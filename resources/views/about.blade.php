fc17f619222b82687ae28004929c08b6b3682abf
KATAKI33 <mas33yoi@gmail.com>
2025-01-17 01:32:23 +0700

    bagian

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -48,10 +48,10 @@
       <div class="container">
         <div class="row">
           <div class="col-md-7">
-            <h1 class="text-white font-weight-bold">About Us</h1>
+            <h1 class="text-white font-weight-bold">Tentang Kita</h1>
             <div class="custom-breadcrumbs">
               <a href="#">Home</a> <span class="mx-2 slash">/</span>
-              <span class="text-white"><strong>About Us</strong></span>
+              <span class="text-white"><strong>tentang kita</strong></span>
             </div>
           </div>
         </div>
@@ -106,15 +106,12 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6 mb-5 mb-lg-0">
-            <a data-fancybox data-ratio="2" href="https://vimeo.com/317571768" class="block__96788">
-              <span class="play-icon"><span class="icon-play"></span></span>
-              <img src="images/sq_img_6.jpg" alt="Image" class="img-fluid img-shadow">
-            </a>
+            <img src="images/bareng.jpg" alt="Image" class="img-fluid mb-4 rounded">
           </div>
           <div class="col-lg-5 ml-auto">
-            <h2 class="section-title mb-3">JobBoard For Freelancers, Web Developers</h2>
-            <p class="lead">Eveniet voluptatibus voluptates suscipit minima, cum voluptatum ut dolor, sed facere corporis qui, ea quisquam quis odit minus nulla vitae. Sit, voluptatem.</p>
-            <p>Ipsum harum assumenda in eum vel eveniet numquam, cumque vero vitae enim cupiditate deserunt eligendi officia modi consectetur. Expedita tempora quos nobis earum hic ex asperiores quisquam optio nostrum sit!</p>
+            <h2 class="section-title mb-3">JobBoard untuk Freelancers, Web Developers</h2>
+            <p class="lead">kami membuat website untuk freelance dan web Developers</p>
+            <p>diwebsite ini kalian bisa mencari freelance dengan gampang</p>
           </div>
         </div>
       </div>
@@ -124,15 +121,12 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6 mb-5 mb-lg-0 order-md-2">
-            <a data-fancybox data-ratio="2" href="https://vimeo.com/317571768" class="block__96788">
-              <span class="play-icon"><span class="icon-play"></span></span>
-              <img src="images/sq_img_8.jpg" alt="Image" class="img-fluid img-shadow">
-            </a>
+            <img src="images/bareng.jpg" alt="Image" class="img-fluid mb-4 rounded">
           </div>
           <div class="col-lg-5 mr-auto order-md-1  mb-5 mb-lg-0">
-            <h2 class="section-title mb-3">JobBoard For Workers</h2>
-            <p class="lead">Eveniet voluptatibus voluptates suscipit minima, cum voluptatum ut dolor, sed facere corporis qui, ea quisquam quis odit minus nulla vitae. Sit, voluptatem.</p>
-            <p>Ipsum harum assumenda in eum vel eveniet numquam, cumque vero vitae enim cupiditate deserunt eligendi officia modi consectetur. Expedita tempora quos nobis earum hic ex asperiores quisquam optio nostrum sit!</p>
+            <h2 class="section-title mb-3">JobBoard bust pekerja</h2>
+            <p class="lead">kami juga membuat tempat untuk mencari pekerjaan lain</p>
+            <p>diwebsite ini kalian bisa mencari pekerjaan yang kalian mau dan banyak lainnya</p>
           </div>
         </div>
       </div>
@@ -143,20 +137,20 @@
       <div class="container">
         <div class="row mb-5">
           <div class="col-12 text-center" data-aos="fade">
-            <h2 class="section-title mb-3">Our Team</h2>
+            <h2 class="section-title mb-3">tim kami</h2>
           </div>
         </div>
 
         <div class="row align-items-center block__69944">
 
           <div class="col-md-6">
-            <img src="images/person_6.jpg" alt="Image" class="img-fluid mb-4 rounded">
+            <img src="images/andhika.jpg" alt="Image" class="img-fluid mb-4 rounded">
           </div>
 
           <div class="col-md-6">
-            <h3>Elisabeth Smith</h3>
-            <p class="text-muted">Creative Director</p>
-            <p>Soluta quasi cum delectus eum facilis recusandae nesciunt molestias accusantium libero dolores repellat id in dolorem laborum ad modi qui at quas dolorum voluptatem voluptatum repudiandae voluptatibus ut? Ex vel  ad explicabo iure ipsa possimus consectetur neque rem molestiae eligendi velit?.</p>
+            <h3>Andhika RIfqi</h3>
+            <p class="text-muted">Web Developers</p>
+            <p>ketua tim pelaksana</p>
             <div class="social mt-4">
               <a href="#"><span class="icon-facebook"></span></a>
               <a href="#"><span class="icon-twitter"></span></a>
@@ -166,13 +160,61 @@
           </div>
 
           <div class="col-md-6 order-md-2 ml-md-auto">
-            <img src="images/person_5.jpg" alt="Image" class="img-fluid mb-4 rounded">
+            <img src="images/abid.jpg" alt="Image" class="img-fluid mb-4 rounded">
           </div>
 
           <div class="col-md-6">
-            <h3>Chintan Patel</h3>
-            <p class="text-muted">Creative Director</p>
-            <p>Soluta quasi cum delectus eum facilis recusandae nesciunt molestias accusantium libero dolores repellat id in dolorem laborum ad modi qui at quas dolorum voluptatem voluptatum repudiandae voluptatibus ut? Ex vel  ad explicabo iure ipsa possimus consectetur neque rem molestiae eligendi velit?.</p>
+            <h3>Muhammad abid</h3>
+            <p class="text-muted">Admin Database</p>
+            <p>pembuat database serta web security</p>
+            <div class="social mt-4">
+              <a href="#"><span class="icon-facebook"></span></a>
+              <a href="#"><span class="icon-twitter"></span></a>
+              <a href="#"><span class="icon-instagram"></span></a>
+              <a href="#"><span class="icon-linkedin"></span></a>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <img src="images/ali.jpg" alt="Image" class="img-fluid mb-4 rounded">
+          </div>
+
+          <div class="col-md-6">
+            <h3>Ali Shahab</h3>
+            <p class="text-muted">Web Developers</p>
+            <p>membatu ketua</p>
+            <div class="social mt-4">
+              <a href="#"><span class="icon-facebook"></span></a>
+              <a href="#"><span class="icon-twitter"></span></a>
+              <a href="#"><span class="icon-instagram"></span></a>
+              <a href="#"><span class="icon-linkedin"></span></a>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <img src="images/shanti.jpg" alt="Image" class="img-fluid mb-4 rounded">
+          </div>
+
+          <div class="col-md-6">
+            <h3>Shanti</h3>
+            <p class="text-muted">Marketing</p>
+            <p>ketua Marketing</p>
+            <div class="social mt-4">
+              <a href="#"><span class="icon-facebook"></span></a>
+              <a href="#"><span class="icon-twitter"></span></a>
+              <a href="#"><span class="icon-instagram"></span></a>
+              <a href="#"><span class="icon-linkedin"></span></a>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <img src="images/nia.jpg" alt="Image" class="img-fluid mb-4 rounded">
+          </div>
+
+          <div class="col-md-6">
+            <h3>Nia</h3>
+            <p class="text-muted">Marketing</p>
+            <p>anggota Marketing</p>
             <div class="social mt-4">
               <a href="#"><span class="icon-facebook"></span></a>
               <a href="#"><span class="icon-twitter"></span></a>
